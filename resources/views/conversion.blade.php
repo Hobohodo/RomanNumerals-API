206cfb245e17e14b6d79f0d705313fb3a0b435ba
Richard Allum <richard.allum@outlook.com>
2017-08-06 15:07:33 +0100

    Remove html templates used for initial testing

--- resources/views/conversion.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Congratulations!</h1>
-
-<p>Your integer <code>{{$conversion->integer}}</code> was converted to <code>{{$conversion->numeral}}</code> successfully!</p>
--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Hello Numeral McNumberFace</h1>
-<p>I am ready to serve</p>
-
-<form action="/convert" method="post">
-    {{csrf_field()}}
-    <input type="number" aria-autocomplete="none" placeholder="Please enter a number sir" name="integer" value="{{$integer}}">
-</form>
